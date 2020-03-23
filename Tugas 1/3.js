e0f0a55dd5763715d3b09498ee87fae5da9cfd4f
bagakibadi <31173796+bagakibadi@users.noreply.github.com>
2020-03-23 11:47:42 +0700

    first commit

--- Tugas 1/3.js
+++ Tugas 1/3.js
@@ -1,6 +1,10 @@
 function pohon(tinggi, tahun) {
+  if(tinggi == 0 || tahun == 0) {
+    console.log('Tinggi dan Tahun harus lebih dari 0');
+  }else {
     for(i = 0; i < tahun; i++){
         console.log(tinggi = tinggi * 2 + 1);
     }
+  }
 }
 pohon(3,2)
